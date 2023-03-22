42ef2f7b3b55449965e10d7db8e968f29e7f4ae3
Kyle Runge <krunge1@hotmail.com>
2023-03-22 18:47:10 -0500

    Daikichi pt 2

--- src/main/java/com/krunge/daikichi/DaikichiController.java
+++ src/main/java/com/krunge/daikichi/DaikichiController.java
@@ -1,6 +1,7 @@
 package com.krunge.daikichi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,20 @@ public class DaikichiController {
 	@GetMapping("/daikichi/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to open to new ideas!";
+	}
+	
+	@RequestMapping("daikichi/travel/{city}")
+	public String showCity(@PathVariable("city")String city) {
+		return "Congratulations! You will soon travel to the beautiful city of " + city;
+	}
+	
+	@RequestMapping("daikichi/lotto/{number}")
+	public String showLotto(@PathVariable("number")int number) {
+		if (number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be wary of tempting offers";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family amd friends";
+		}
 	}
 }
